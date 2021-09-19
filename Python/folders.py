8571b7b8457f62e49977fb741eaa2caaed2660a0
leonknight99 <67868995+leonknight99@users.noreply.github.com>
2021-09-19 15:47:56 +0100

    Start of the project and Python code

---
+++ Python/folders.py
@@ -0,0 +1,35 @@
+import os
+
+
+def path_input():
+    while True:
+        folder_path = str(input('\nInsert path of folder: '))
+        if os.path.isdir(folder_path):
+            break
+        print('Error: Incorrect path to a folder inserted')
+
+    return folder_path
+
+
+def directory_to_dictionary(folder_path):
+    for (root, subDirectories, files) in os.walk(folder_path):
+        print(root)
+        print(subDirectories)
+        print(files)
+        print('-----------------------')
+        
+
+def compare_folders():
+    while True:
+        nFolders = input('\nEnter number of folders to compare: ')
+        if nFolders.isnumeric():
+            nFolders = int(nFolders)
+            break
+        print('Error: A positive interger has not been entered')
+    
+    pathToFolders = [path_input() for n in range(nFolders)]
+    for p in pathToFolders:
+        print(os.walk(p))
+
+
+directory_to_dictionary('/Volumes/SANDISK128/Music/Loessless')
